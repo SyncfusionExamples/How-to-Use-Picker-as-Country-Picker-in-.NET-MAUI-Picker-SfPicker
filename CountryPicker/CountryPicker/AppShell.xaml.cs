3646dbf50374b5bad57125e2004d8c2885f1a56a
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2023-12-07 12:40:23 +0530

    Added sample.

---
+++ CountryPicker/CountryPicker/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CountryPicker;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CountryPicker/CountryPicker/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CountryPicker;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
