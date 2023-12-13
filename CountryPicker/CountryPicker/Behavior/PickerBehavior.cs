918f836ad63a4d7aa9fff1003a8b4a4a6220421f
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2023-12-13 17:03:03 +0530

    Updated sample with MVVM structure.

---
+++ CountryPicker/CountryPicker/Behavior/PickerBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Syncfusion.Maui.Picker;
+using System.Collections.ObjectModel;
+
+namespace CountryPicker
+{
+    public class PickerBehavior : Behavior<ContentPage>
+    {
+        private SfPicker picker;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.picker = bindable.FindByName<SfPicker>("picker");
+            
+            ObservableCollection<object> countryName = new ObservableCollection<object>();
+            countryName.Add("UnitedStates");
+            countryName.Add("Qatar");
+            countryName.Add("India");
+            countryName.Add("Germany");
+            countryName.Add("Spain");
+            countryName.Add("Malaysia");
+            countryName.Add("Netherlands");
+            countryName.Add("Mexico");
+            countryName.Add("Brazil");
+            PickerColumn pickerColumn = new PickerColumn()
+            {
+                ItemsSource = countryName,
+                SelectedIndex = 3,
+            };
+
+            this.picker.Columns.Add(pickerColumn);
+        }
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            if (this.picker != null)
+            {
+                this.picker = null;
+            }
+        }
+    }
+
+}
--- CountryPicker/CountryPicker/MainPage.xaml.cs
+++ CountryPicker/CountryPicker/MainPage.xaml.cs
@@ -1,28 +1,9 @@
-﻿using Syncfusion.Maui.Picker;
-using System.Collections.ObjectModel;
-
-namespace CountryPicker;
+﻿namespace CountryPicker;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
-        ObservableCollection<object> cityName = new ObservableCollection<object>();
-        cityName.Add("UnitedStates");
-        cityName.Add("Qatar");
-        cityName.Add("India");
-        cityName.Add("Germany");
-        cityName.Add("Spain");
-        cityName.Add("Malaysia");
-        cityName.Add("Netherlands");
-        cityName.Add("Mexico");
-        cityName.Add("Brazil");
-        PickerColumn pickerColumn = new PickerColumn()
-        {
-            ItemsSource = cityName,
-            SelectedIndex = 1,
-        };
-        picker.Columns.Add(pickerColumn);
     }
 }
